632b4b11f1d108f0263e6680dfdd119d8712ce13
Boe Reh <justboe@proton.me>
2025-12-17 18:23:01 -0600

    fix some env var issues and packages

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -5,7 +5,7 @@ import { DATABASE_URL, DATABASE_AUTH_TOKEN } from '$env/static/private';
 import { dev } from '$app/environment';
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!dev && DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+if (!dev && !DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
 
 const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
 
